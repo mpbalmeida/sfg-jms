a5736f71b26829f2678a1597f6fbbfa27f433ddd
Marcos Almeida <marcos@marcosalmeida.dev>
2021-04-26 23:12:54 +0200

    chore: sample task executor with a hello world message

---
+++ src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    private static final long serialVersionUID = 5415670383497139273L;
+
+    private UUID id;
+    private String message;
+}
